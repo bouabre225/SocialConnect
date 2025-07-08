cba2d6ab2f6c0d357c9e2ce3967e3839eff5e98f
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-08 15:13:37 +0200

    ajout

--- api/users/home.php
+++ api/users/home.php
@@ -2,6 +2,12 @@
 //congig
 require '../../api/config.php';
 
+// Inclure le chargeur automatique de Composer pour JWT
+require_once '../../vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Exception;
+
 //inclusion des headers
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     // Configuration des headers
@@ -20,8 +26,23 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     jsonResponse(['status' => 'error', 'message' => 'Méthode non autorisée'], 405);
 }
 
-//creer un post 
+// Clé secrète pour JWT
+$secretKey = 'your-secret-key';
 
+// Fonction pour vérifier le token JWT
+function authentificateToken($headers){
+    global $secretKey;
+    if (!isset($headers['Authorization'])) {
+        jsonResponse(['status' => 'error', 'message' => 'Aucun token fourni'], 401);
+    }
+    $token = str_replace('Bearer ', '', $headers['Authorization']);
+    try {
+        $decoded = JWT::decode($token, new Key($secretKey, 'HS256'));
+        return $decoded;
+    } catch (Exception $e) {
+        jsonResponse(['status' => 'error', 'message' => 'Token invalide'], 401);
+    }
+}
 
 
 
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '168298e2ddb3df397e7adeedb2337e1ccabf4c4c',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,11 +11,20 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '168298e2ddb3df397e7adeedb2337e1ccabf4c4c',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.11.1',
+            'version' => '6.11.1.0',
+            'reference' => 'd1e91ecf8c598d073d0995afa8cd5c75c6e19e66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
