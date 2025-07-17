351b51b5ff665ff80d18cae3700f9a9cdb3cfc95
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-18 00:44:06 +0200

    changement des variables

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,10 @@
 // config.js
 
 // URL de ton API
-const API_URL = 'http://localhost/api/users/home';
-const API_URL1 = 'http://localhost/api/users/chat';
-const API_URL2 = 'http://localhost/ReseauSocial/api';
-const WS_URL = 'ws://localhost:8080';
+const API_URL = 'https://socialconnect-94gz.onrender.com/api/users/home';
+const API_URL1 = 'https://socialconnect-94gz.onrender.com/api/users/chat';
+const API_URL2 = 'https://socialconnect-94gz.onrender.com/api';
+const WS_URL = 'ws://socialconnect-94gz.onrender.com:8080';
 console.log(API_URL1);
 console.log(API_URL);
 console.log(API_URL2);
