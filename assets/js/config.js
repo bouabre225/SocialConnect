22124b574f215b68ea36f214dd99afe0b7974d8b
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-18 01:24:27 +0200

    modif

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,12 +1,12 @@
 // config.js
 
 // URL de ton API
-const API_URL = 'http://localhost:8001/users';
-const API_URL1 = 'http://localhost:8001/api/users';
-const API_URL2 = 'http://localhost:8001/api/';
-const API_URL3 = 'http://localhost:8001/admin/'; 
-const API_URL4 = 'http://localhost:8001/';
-const WS_URL = 'ws://localhost:8002';
+const API_URL = 'http://social8connect.netlify.app:8001/users';
+const API_URL1 = 'http://social8connect.netlify.app:8001/api/users';
+const API_URL2 = 'http://social8connect.netlify.app:8001/api/';
+const API_URL3 = 'http://social8connect.netlify.app:8001/admin/'; 
+const API_URL4 = 'http://social8connect.netlify.app:8001/';
+const WS_URL = 'ws://social8connect.netlify.app:8002';
 
 console.log(API_URL1);
 console.log(API_URL);
