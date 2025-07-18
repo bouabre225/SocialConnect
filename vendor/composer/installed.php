09b4c2768f2698b723514b8f2c4e49f05b75c655
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-18 13:23:29 +0200

    modif

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'd4f05dff69be83ddad012a42735498d411a545c6',
+        'reference' => 'dad2f1f73b88d043a2d117f8798a4a28c5099376',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'd4f05dff69be83ddad012a42735498d411a545c6',
+            'reference' => 'dad2f1f73b88d043a2d117f8798a4a28c5099376',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
