d12c1bb29b8a7e3862bdee742a950fe83a0e00a2
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-07-18 13:30:02 +0200

    modif

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,11 @@
 // config.js
 
 // URL de ton API
-const API_URL = 'https://socialconnect-94gz.onrender.com/users';
+const API_URL = 'https://socialconnect-94gz.onrender.com/api/users';
 const API_URL1 = 'https://socialconnect-94gz.onrender.com/api/users';
 const API_URL2 = 'https://socialconnect-94gz.onrender.com/api/';
 const API_URL3 = 'https://socialconnect-94gz.onrender.com/admin/'; 
-const API_URL4 = 'https://socialconnect-94gz.onrender.com/';
+const API_URL4 = 'https://socialconnect-94gz.onrender.com/api/';
 const WS_URL = 'ws://socialconnect-94gz.onrender.com:8002';
 
 console.log(API_URL1);
